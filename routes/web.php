5adab287f43eb42a00aaf872acbb746693e7c746
陳姿穎 <roeyang1410@gmail.com>
2017-11-24 20:20:38 +0800

    練習十

--- routes/web.php
+++ routes/web.php
@@ -73,13 +73,13 @@ Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'],function() {
         Route::get('{student_no}',[
          'as'=>'student',
-         'users'=>function($student_no){
+         'uses'=>function($student_no){
                     return "學號：".$student_no;
          }
      ]);
      Route::get('{student_no}/score/{subject?}',[
          'as'=>'student.score',
-         'users'=>function($student_no,$subject=null){
+         'uses'=>function($student_no,$subject=null){
                     return "學號：".$student_no.' 的'.((is_null($subject))?'所有科目':$subject).'成績';
          }
      ])->where(['subject'=>'(chinese | english | math']);
@@ -99,3 +99,5 @@ Route::group(['prefix' => 'student'],function(){
                 'as' => 'student.score',
                 'uses' => 'StudentController@getStudentScore'])->where(['subject' => '(chinese|english|math)']);
 });
+
+Route::get('cool', 'Cool\TestController@indexc');
