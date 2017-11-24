b09b85cc8952bfd3752f390abe9a92feb8059bd7
陳姿穎 <roeyang1410@gmail.com>
2017-11-24 19:00:35 +0800

    練習七：路由命名

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
         return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
  })->where(['subject'=>'(chinese | english | math']);
 */
-
+/*
 //練習六
 Route::pattern('student_no','s[0-9{10}');
 Route::group(['prefix'=>'student',function(){
@@ -67,3 +67,21 @@ Route::group(['prefix'=>'student',function(){
          return "學號：" . $student_no . "的" . ((is_null($subject))? "所有科目": $subject)."成績";
      })->where(['subject'=>'(chinese | english | math)']);
  }]);
+*/
+
+//練習七
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'],function() {
+        Route::get('{student_no}',[
+         'as'=>'student',
+         'users'=>function($student_no){
+                    return "學號：".$student_no;
+         }
+     ]);
+     Route::get('{student_no}/score/{subject?}',[
+         'as'=>'student.score',
+         'users'=>function($student_no,$subject=null){
+                    return "學號：".$student_no.' 的'.((is_null($subject))?'所有科目':$subject).'成績';
+         }
+     ])->where(['subject'=>'(chinese | english | math']);
+ });
